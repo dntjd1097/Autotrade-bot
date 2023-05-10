c1da79707d65f3d07139007e5f1bb3c5e7c5a348
dntjd1097 <dntjd1097@gmail.com>
2023-05-10 12:15:35 +0900

    commit

--- Autotrade.py
+++ Autotrade.py
@@ -105,126 +105,126 @@ while True:
         start_time = get_start_time("KRW-BTC")
         end_time = start_time + timedelta(seconds=120)
         get_balance()
-        if start_time < now < end_time:
-            current_price = get_current_price("KRW-BTC")
-            ma1 = get_moving_average(ticker, 1)
-            ma15 = get_moving_average(ticker, 15)
-            ma20 = get_moving_average(ticker, 20)
-            ma30 = get_moving_average(ticker, 30)
-            data = [
-                get_moving_average(ticker, 1),
-                get_moving_average(ticker, 15),
-                get_moving_average(ticker, 20),
-                get_moving_average(ticker, 30),
-            ]
-            min_target_price = min(data)
-            max_target_price = max(data)
-            check_buy = current_price < min_target_price
-            check_sell = current_price > max_target_price
-            post_message(
-                myToken,
-                "#coin",
-                "*"
-                + coin
-                + "*\n *현재가* : "
-                + str(int(current_price))
-                + "\n 구매해야됨 :"
-                + str(check_buy)
-                + " ["
-                + str(int(min_target_price))
-                + "]"
-                + "\n 판매해야됨 :"
-                + str(check_sell)
-                + " ["
-                + str(int(max_target_price))
-                + "]",
-            )
-            # check1 = target_price > current_price
-            # check2 = ma15 > current_price
-            # check3 = "X"
-            # if check1 and check2:
-            #     check3 = "O"
 
-            # if check1:
-            #     check3 = "O"
-            #     mark = "▼"
-            #     # mark1=""
-            #     mark1 = ""
-            #     required = " X "
-            #     # required=format(int(target_price)-int(current_price), ",")
+        current_price = get_current_price("KRW-BTC")
+        ma1 = get_moving_average(ticker, 1)
+        ma15 = get_moving_average(ticker, 15)
+        ma20 = get_moving_average(ticker, 20)
+        ma30 = get_moving_average(ticker, 30)
+        data = [
+            get_moving_average(ticker, 1),
+            get_moving_average(ticker, 15),
+            get_moving_average(ticker, 20),
+            get_moving_average(ticker, 30),
+        ]
+        min_target_price = min(data)
+        max_target_price = max(data)
+        check_buy = current_price < min_target_price
+        check_sell = current_price > max_target_price
+        post_message(
+            myToken,
+            "#coin",
+            "*"
+            + coin
+            + "*\n *현재가* : "
+            + str(int(current_price))
+            + "\n 구매해야됨 :"
+            + str(check_buy)
+            + " ["
+            + str(int(min_target_price))
+            + "]"
+            + "\n 판매해야됨 :"
+            + str(check_sell)
+            + " ["
+            + str(int(max_target_price))
+            + "]",
+        )
+        # check1 = target_price > current_price
+        # check2 = ma15 > current_price
+        # check3 = "X"
+        # if check1 and check2:
+        #     check3 = "O"
 
-            # else:
-            #     # mark = "▼"
-            #     mark = "▲"
-            #     mark1 = "▼"
-            #     # mark1 = "▲"
+        # if check1:
+        #     check3 = "O"
+        #     mark = "▼"
+        #     # mark1=""
+        #     mark1 = ""
+        #     required = " X "
+        #     # required=format(int(target_price)-int(current_price), ",")
 
-            #     required = ""
-            #     required = format(int(current_price) - int(target_price), ",")
+        # else:
+        #     # mark = "▼"
+        #     mark = "▲"
+        #     mark1 = "▼"
+        #     # mark1 = "▲"
 
-            # format(,",")
-            # post_message(
-            #     myToken,
-            #     "#coin_test",
-            #     "현재시간 : "
-            #     + str(now)
-            #     + "\nBTC target_price : "
-            #     + format(int(target_price), ",")
-            #     + "\nBTC ma15 : "
-            #     + format(int(ma15), ",")
-            #     + "\nBTC current_price : "
-            #     + format(int(current_price), ",")
-            #     + "  "
-            #     + str(mark)
-            #     + "\n"
-            #     + str(check3)
-            #     + "   [required ="
-            #     + required
-            #     + mark1
-            #     + "  ]",
-            # )
+        #     required = ""
+        #     required = format(int(current_price) - int(target_price), ",")
 
-            if check_buy:  # and check2:
-                if krw > 5000:
+        # format(,",")
+        # post_message(
+        #     myToken,
+        #     "#coin_test",
+        #     "현재시간 : "
+        #     + str(now)
+        #     + "\nBTC target_price : "
+        #     + format(int(target_price), ",")
+        #     + "\nBTC ma15 : "
+        #     + format(int(ma15), ",")
+        #     + "\nBTC current_price : "
+        #     + format(int(current_price), ",")
+        #     + "  "
+        #     + str(mark)
+        #     + "\n"
+        #     + str(check3)
+        #     + "   [required ="
+        #     + required
+        #     + mark1
+        #     + "  ]",
+        # )
+
+        if check_buy:  # and check2:
+            if krw > 5000:
+                post_message(
+                    myToken,
+                    "#coin_test",
+                    "\n 구매시작 " + "\n==================================",
+                )
+                buy_result = upbit.buy_market_order(ticker, krw * 0.9995)
+                post_message(
+                    myToken, "#coin", "\n" + coin + " buy : " + str(buy_result)
+                )
+                get_balances_coin()
+
+        if BTC_balance > 0.00008:
+            if check_sell:
+                fee = BTC_avg_buy_price / (1 + comission)
+                if fee < current_price:
+                    sell_result = upbit.sell_market_order(ticker, BTC_balance)
                     post_message(
                         myToken,
-                        "#coin_test",
-                        "\n 구매시작 " + "\n==================================",
+                        "#coin",
+                        "\n" + coin + " sell : " + str(sell_result),
                     )
-                    buy_result = upbit.buy_market_order(ticker, krw * 0.9995)
                     post_message(
-                        myToken, "#coin", "\n" + coin + " buy : " + str(buy_result)
+                        myToken,
+                        "#coin_reward",
+                        "현재시간 : "
+                        + str(now)
+                        + "\n[보유현금 : "
+                        + format(krw, ",")
+                        + "krw ]"
+                        + "\n==================================",
                     )
-                    get_balances_coin()
+                    get_balances_krw()
+        # elif fee < current_price:
+        #    post_message(myToken,"#coin_test",
+        #    "매도 X : required(" +str(current_price-fee)+
+        #    " ▲ )"
+        #    )
 
-            if BTC_balance > 0.00008:
-                if check_sell:
-                    fee = BTC_avg_buy_price / (1 + comission)
-                    if fee < current_price:
-                        sell_result = upbit.sell_market_order(ticker, BTC_balance)
-                        post_message(
-                            myToken,
-                            "#coin",
-                            "\n" + coin + " sell : " + str(sell_result),
-                        )
-                        post_message(
-                            myToken,
-                            "#coin_reward",
-                            "현재시간 : "
-                            + str(now)
-                            + "\n[보유현금 : "
-                            + format(krw, ",")
-                            + "krw ]"
-                            + "\n==================================",
-                        )
-                        get_balances_krw()
-            # elif fee < current_price:
-            #    post_message(myToken,"#coin_test",
-            #    "매도 X : required(" +str(current_price-fee)+
-            #    " ▲ )"
-            #    )
-
-            time.sleep(120)
+        break
     except Exception as e:
         print(e)
         post_message(myToken, "#coin_error", e)
